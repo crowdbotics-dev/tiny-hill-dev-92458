24e5968001aa9692ddd5b6294d06242cf383b82f
Crowdbotics <engineering@crowdbotics.com>
2023-06-17 23:17:08 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "tiny-hill-dev-92458",
+  url: "https://tiny-hill-dev-92458.botics.co",
+  api: "https://tiny-hill-dev-92458.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
